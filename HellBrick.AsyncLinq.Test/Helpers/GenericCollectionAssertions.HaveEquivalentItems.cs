912aa22df1e9ccb93b17105c98133333d8e6dcb6
HellBrick <hellbrick@outlook.com>
2017-08-27 15:50:17 +0300

    Added ICollection<T>.Should().HaveEquivalentItems() helper

---
+++ HellBrick.AsyncLinq.Test/Helpers/GenericCollectionAssertions.HaveEquivalentItems.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using FluentAssertions.Collections;
+
+namespace HellBrick.AsyncLinq.Test
+{
+	internal static class GenericCollectionAssertions
+	{
+		public static AndConstraint<GenericCollectionAssertions<T>> HaveEquivalentItems<T>( this GenericCollectionAssertions<T> should, IReadOnlyCollection<T> expectedCollection )
+			=> should.HaveSameCount( expectedCollection )
+			.And.StartWith( expectedCollection );
+	}
+}
