b6129467719c1191b354f9283b7dee9f33fbb29c
HellBrick <hellbrick@outlook.com>
2017-08-27 15:50:33 +0300

    Added TaskAsyncEnumerator<T> helper

---
+++ HellBrick.AsyncLinq.Test/Helpers/TaskAsyncEnumerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace HellBrick.AsyncLinq.Test.Helpers
+{
+	internal class TaskAsyncEnumerator<T> : IAsyncEnumerator<T>
+	{
+		private readonly Task<T>[] _tasks;
+		private int _tasksEnumerated = 0;
+
+		public TaskAsyncEnumerator( params Task<T>[] tasks ) => _tasks = tasks;
+
+		public Task<Optional<T>> GetNextAsync()
+			=> _tasksEnumerated < _tasks.Length
+			? _tasks[ _tasksEnumerated++ ].ContinueWith( t => new Optional<T>( t.GetAwaiter().GetResult() ) )
+			: Optional<T>.NoValueTask;
+	}
+}
